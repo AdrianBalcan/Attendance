00eecd1c6257bd048cef41999d472c9b584d409a
AdrianBalcan <contact@adrianbalcan.com>
2017-01-18 19:17:04 +0200

    fixed select2

--- lib/web/attendance/web/controllers/page_controller.ex
+++ lib/web/attendance/web/controllers/page_controller.ex
@@ -3,7 +3,8 @@ defmodule Attendance.PageController do
   plug Attendance.Plug.Authenticate
 
   def index(conn, _params) do
-    employeesStatus = Repo.all(from a in Attendance.Attendance, join: e in Attendance.Employee ,on: a.employeeID == e.id, select: %{firstname: e.firstname, lastname: e.lastname, employeeID: a.employeeID, status: a.status, inserted_at: a.inserted_at}, distinct: a.employeeID, order_by: [desc: :id])
+    employeesStatus = Repo.all(from a in Attendance.Attendance, right_join: e in Attendance.Employee ,on: e.id == a.employeeID, select: %{firstname: e.firstname, lastname: e.lastname, employeeID: a.employeeID, status: a.status, inserted_at: a.inserted_at}, distinct: e.id, order_by: [desc: :id])
+    IO.inspect employeesStatus
     render(conn, "index.html", employeesStatus: employeesStatus)
   end
 end
